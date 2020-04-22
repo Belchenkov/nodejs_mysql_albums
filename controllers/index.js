5962d96074f10cb3e56b03d2df748e968b35f93a
Aleksey Belchenkov <u608110@gmail.com>
2020-04-22 20:48:02 +0300

    Update Index  Controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,25 @@
-// Index controller
-exports.show = function (req, res) {
-    // Show index content
+// List Sample Bands
+exports.show = (req, res) => {
+    // List all comments and sort by Date
+    const topBands = [
+        {
+            name: 'Motorhead',
+            description: 'Rock and Roll Band',
+            album: 'http://s2.vagalume.com/motorhead/discografia/orgasmatron-W320.jpg', year:'1986',
+        },
+        {
+            name: 'Judas Priest',
+            description: 'Heavy Metal band',
+            album: 'http://s2.vagalume.com/judas-priest/discografia/screaming-for-vengeance-W320.jpg', year:'1982',
+        },
+        {
+            name: 'Ozzy Osbourne',
+            description: 'Heavy Metal Band',
+            album: 'http://s2.vagalume.com/ozzy-osbourne/discografia/diary-of-a-madman-W320.jpg', year:'1981',
+        }
+    ];
     res.render('index', {
-        title: 'Express'
+        title: 'The best albums of the eighties',
+        callToAction: 'Please be welcome, click the button below and register your favorite album.', bands: topBands
     });
 };
